1e7a8fab9f1def719e8b83d40aba18277221450a
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-08-18 16:10:31 +0200

    Fix page documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+//! Implementations of various algorithms and data structures.
+//!
+//! ## Sorting algorithms
+//! - Insertion sort
 pub mod sorting;
--- src/sorting.rs
+++ src/sorting.rs
@@ -1,25 +1,25 @@
-/// # Sorting
-/// This module contains sorting algorithms.
-/// ## Example
-/// Using sorting algorithm method directly:
-/// ```rust
-/// use rusty_algo::sorting::Sortable;
-/// let mut arr = vec![3, 2, 1];
-/// arr.insertion_sort();
-/// assert_eq!(arr, vec![1, 2, 3]);
-/// ```
-///
-/// Using sorting algorithm enum:
-/// ```rust
-/// use rusty_algo::sorting::SortAlgorithm::InsertionSort;
-/// use rusty_algo::sorting::Sortable;
-/// let mut arr = vec![3, 2, 1];
-/// arr.sort(&InsertionSort);
-/// assert_eq!(arr, vec![1, 2, 3]);
-/// ```
-///
-/// ## Sorting algorithms
-/// - Insertion sort
+//! # Sorting
+//! This module contains sorting algorithms.
+//! ## Example
+//! Using sorting algorithm method directly:
+//! ```rust
+//! use rusty_algo::sorting::Sortable;
+//! let mut arr = vec![3, 2, 1];
+//! arr.insertion_sort();
+//! assert_eq!(arr, vec![1, 2, 3]);
+//! ```
+//!
+//! Using sorting algorithm enum:
+//! ```rust
+//! use rusty_algo::sorting::SortAlgorithm::InsertionSort;
+//! use rusty_algo::sorting::Sortable;
+//! let mut arr = vec![3, 2, 1];
+//! arr.sort(&InsertionSort);
+//! assert_eq!(arr, vec![1, 2, 3]);
+//! ```
+//!
+//! ## Sorting algorithms
+//! - Insertion sort
 
 /// Sortable trait for sorting algorithms
 pub trait Sortable {
